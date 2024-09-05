4b2c3aa6273f48eb02ec1dd94e82cb792209beb1
CofCat456 <a25690756@gmail.com>
2024-09-05 09:28:24 +0800

    fix: avoid err_ngrok_6024

--- src/lib/ky.ts
+++ src/lib/ky.ts
@@ -4,4 +4,7 @@ const { VITE_API_BASE_URL } = import.meta.env
 
 export default kyStandard.create({
   prefixUrl: VITE_API_BASE_URL,
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+  },
 })
